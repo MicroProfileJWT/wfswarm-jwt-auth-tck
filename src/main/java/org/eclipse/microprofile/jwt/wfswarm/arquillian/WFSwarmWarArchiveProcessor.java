5268422561f554aa4accf584fbf2f7404d0d1657
Scott Stark <starksm64@gmail.com>
2017-08-06 10:54:08 -0700

    Initial MP-JWT TCK harness artifact for Wildfly-Swarm
    Signed-off-by: Scott Stark <starksm64@gmail.com>

---
+++ src/main/java/org/eclipse/microprofile/jwt/wfswarm/arquillian/WFSwarmWarArchiveProcessor.java
@@ -0,0 +1,28 @@
+package org.eclipse.microprofile.jwt.wfswarm.arquillian;
+
+import java.util.logging.Logger;
+
+import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
+import org.jboss.arquillian.test.spi.TestClass;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+public class WFSwarmWarArchiveProcessor implements ApplicationArchiveProcessor {
+    private static Logger log = Logger.getLogger(WFSwarmWarArchiveProcessor.class.getName());
+
+    @Override
+    public void process(Archive<?> appArchive, TestClass testClass) {
+        if (!(appArchive instanceof WebArchive)) {
+            return;
+        }
+        log.info("Preparing archive: "+appArchive);
+        WebArchive war = WebArchive.class.cast(appArchive);
+        war.addAsResource("project-defaults.yml", "/project-defaults.yml")
+            .addAsWebInfResource("jwt-users.properties", "classes/jwt-users.properties")
+            .addAsWebInfResource("jwt-roles.properties", "classes/jwt-roles.properties")
+            .addAsWebInfResource("WEB-INF/jboss-web.xml", "jboss-web.xml")
+            .addAsManifestResource(war.get("/WEB-INF/classes/publicKey.pem").getAsset(), "/MP-JWT-SIGNER")
+                ;
+        log.info("Augmented war: \n"+war.toString(true));
+    }
+}
---
+++ src/main/java/org/eclipse/microprofile/jwt/wfswarm/tck/TCKTokenParser.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016-2017 Contributors to the Eclipse Foundation
+ *
+ *  See the NOTICE file(s) distributed with this work for additional
+ *  information regarding copyright ownership.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.eclipse.microprofile.jwt.wfswarm.tck;
+
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.eclipse.microprofile.jwt.JWTPrincipal;
+import org.eclipse.microprofile.jwt.impl.DefaultJWTCallerPrincipalFactory;
+import org.eclipse.microprofile.jwt.principal.JWTAuthContextInfo;
+import org.eclipse.microprofile.jwt.principal.JWTCallerPrincipalFactory;
+import org.eclipse.microprofile.jwt.tck.util.ITokenParser;
+
+/**
+ * MP-JWT TCK harness class to parse a token string
+ */
+public class TCKTokenParser implements ITokenParser {
+
+
+    @Override
+    public JWTPrincipal parse(String bearerToken, String issuer, PublicKey publicKey) throws Exception {
+        JWTAuthContextInfo authContextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, issuer);
+        JWTCallerPrincipalFactory factory = DefaultJWTCallerPrincipalFactory.instance();
+        return factory.parse(bearerToken, authContextInfo);
+    }
+
+}
